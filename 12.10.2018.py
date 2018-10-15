609ecaab8a5eee31598810ec97b0c2173fbfeaa4
Erkaiym <43886202+Erkaiym@users.noreply.github.com>
2018-10-15 17:49:18 +0600

    Update 12.10.2018.py

--- 12.10.2018.py
+++ 12.10.2018.py
@@ -36,15 +36,15 @@ file4.close()
 #second way
 with open('kyrgyzstan.txt', 'r') as file1:
     with open('wikipedia.txt', 'w') as file2:
-	    file2.write(file1.read())
+        file2.write(file1.read())
 print('Копирование завершено.')
 
 with open('wikipedia.txt', 'a') as file3:
-	file3.write('The Kyrgyz, a Muslim Turkic people, constitute more than half the population. The history of the Kyrgyz in what is now Kyrgyzstan dates at least to the 17th century. Kyrgyzstan, known under Russian and Soviet rule as Kirgiziya, was conquered by tsarist Russian forces in the 19th century. Formerly a constituent (union) republic of the U.S.S.R., Kyrgyzstan declared its independence on August 31, 1991.')
+    file3.write('The Kyrgyz, a Muslim Turkic people, constitute more than half the population. The history of the Kyrgyz in what is now Kyrgyzstan dates at least to the 17th century. Kyrgyzstan, known under Russian and Soviet rule as Kirgiziya, was conquered by tsarist Russian forces in the 19th century. Formerly a constituent (union) republic of the U.S.S.R., Kyrgyzstan declared its independence on August 31, 1991.')
 print('Файл дополнен.')
 
 with open('wikipedia.txt', 'r') as file4:
-	print(file4.read())
+    print(file4.read())
 
 
 print('\nЗадача № 3')
@@ -57,6 +57,6 @@ print('Копиравание завершено.')
 
 #second way
 with open('conor.jpg', 'rb') as file1:
-	with open('chicken.jpg', 'wb') as file2:
-	    file2.write(file1.read())
+    with open('chicken.jpg', 'wb') as file2:
+        file2.write(file1.read())
 print('Копиравание завершено.')
