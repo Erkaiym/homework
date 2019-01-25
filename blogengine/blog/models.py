f386fbb0091e4e881445b477eea81fcc3b88a57f
Erkaiym <akylova.erkaiym@gmail.com>
2019-01-26 04:58:17 +0600

    9th lesson's done

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -24,6 +24,10 @@ class Post(models.Model):
         return reverse('post_detail_url', kwargs={'slug':self.slug})
 
 
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug':self.slug})
+
+
     def __str__(self):
         return self.title
 
@@ -40,6 +44,10 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug':self.slug})
+
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug':self.slug})
 
 
     def __str__(self):
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -23,6 +23,12 @@ class PostCreate(ObjectCreateMixin, View):
     template = 'blog/post_create_form.html'
 
 
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
+
 
 def tags_list(request):
     tags = Tag.objects.all()
@@ -39,8 +45,7 @@ class TagCreate(ObjectCreateMixin, View):
     template = 'blog/tag_create.html'
 
 
-class TagUpdate(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(instance=tag)
-        return render(request, 'blog/tag_update_form.html', context={'form':bound_form, 'tag':tag})
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
